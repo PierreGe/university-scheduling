5e0a0cb19cd507af8ae6043440c1e1e453120b92
t00n <antoine.carpentier.info@gmail.com>
2015-05-10 16:29:38 +0200

    [add] Writer

---
+++ src/Writer.cpp
@@ -0,0 +1,37 @@
+//
+//  Writer.cpp
+//  InfoFond
+//
+//  Created by Nicolas Omer on 20/05/14.
+//  Copyright (c) 2014 Nicolas Omer. All rights reserved.
+//
+
+#include "Writer.h"
+
+void writeInOutputFile(std::string filename, std::string data){
+    
+    std::ofstream fichier(filename, std::ios::out | std::ios::app);
+    
+    if(fichier)
+    {
+        fichier << data;
+        
+        fichier.close();
+    }
+    else
+        std::cerr << "Impossible d'ouvrir le fichier" << std::endl;
+}
+
+void writeInOutputFile(std::string filename, int number){
+    
+    std::ofstream fichier(filename, std::ios::out | std::ios::app);
+    
+    if(fichier)
+    {
+        fichier << number;
+        
+        fichier.close();
+    }
+    else
+        std::cerr << "Impossible d'ouvrir le fichier" << std::endl;
+}
