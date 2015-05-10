37ee6b6924e473dbe492c5516ca505a16846dcc1
Pierre Gerard <piegerar@ulb.ac.be>
2015-05-10 16:27:34 +0200

    reset writer

---
+++ src/Writer.hpp
@@ -0,0 +1,18 @@
+//
+//  Writer.h
+//  InfoFond
+//
+//  Created by Nicolas Omer on 20/05/14.
+//  Copyright (c) 2014 Nicolas Omer. All rights reserved.
+//
+
+#ifndef __InfoFond__Writer__
+#define __InfoFond__Writer__
+
+#include <iostream>
+#include <fstream>
+
+void writeInFile(std::string, std::string);
+void writeInFile(std::string, int);
+
+#endif /* defined(__InfoFond__Writer__) */
