c412e3389eb3c75cedee2858fed49b9549db169e
t00n <antoine.carpentier.info@gmail.com>
2015-05-14 16:23:20 +0200

    [fix] constraint examen duration on q5

--- src/Problem5.cpp
+++ src/Problem5.cpp
@@ -21,34 +21,20 @@ void Problem5::setConstraints(){
     Problem4::setConstraints();
     this->_constraints.erase("examen_temps_max");
     this->_constraints["examen_duree"] = [this]() {
-        FOR(x, 1, this->_specs.X) {
-            FOR(t1, 1, D(x)) {
-                vec<Lit> lits;
-                for(int t2 = 0; t2 <= this->_specs.T; t2 += D(x)) {
-                    if (t1 + t2 <= this->_specs.T) {
-                        FOR(s, 1, this->_specs.S) {
-                            lits.push(Lit(this->_props[x][s][t1+t2]));
+        FOR(x1, 1, this->_specs.X) {
+            FOR(x2, 1, this->_specs.X) {
+                if (x1 != x2) {
+                FOR(s, 1, this->_specs.S) {
+                    FOR(t1, 1, this->_specs.T) {
+                        FOR(t2, 1, this->_specs.T) {
+                            if (t1 <= t2 + D(x2)-1 and t1 + D(x1)-1 >= t2) {
+                                this->_solver.addBinary(~Lit(this->_props[x1][s][t1]),                    ~Lit(this->_props[x2][s][t2]));
+                            }
                         }
                     }
                 }
-                this->_solver.addClause(lits);
-
+            }
             }
         }
     };
-
-    // Chaque examen dans au plus une salle
-       _constraints["examen_salle_max"] = [this]() {
-               FOR(x, 1, this->_specs.X) {
-                   FOR(t1, 1, this->_specs.T) {
-                       FOR(t2, 1, this->_specs.T) {
-                           FOR(s1, 1, this->_specs.S) {
-                               FOR(s2, s1 + 1, this->_specs.S) {
-                                   this->_solver.addBinary(~Lit(this->_props[x][s1][t1]), ~Lit(this->_props[x][s2][t2]));
-                               }
-                           }
-                       }
-                   }
-               }
-           };
 }
