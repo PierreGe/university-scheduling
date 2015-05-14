08a33581c34593b231a69c026f1562280ad7dc4f
t00n <antoine.carpentier.info@gmail.com>
2015-05-14 16:49:15 +0200

    [add] Q5 OVER

--- src/Problem5.cpp
+++ src/Problem5.cpp
@@ -36,5 +36,51 @@ void Problem5::setConstraints(){
             }
             }
         }
+    };// Un étudiant a au plus un examen à chaque moment
+    _constraints["etudiant_temps_max"] = [this]() {
+        FOR(e, 1, this->_specs.E) {
+            FOR(t1, 1, this->_specs.T) {
+                FOR(t2, 1, this->_specs.T) {
+                    FOR(s, 1, this->_specs.S) {
+                        FOR(x1, 1, this->_specs.X) {
+                            FOR(x2, x1 + 1, this->_specs.X) {
+                                if (A(e, x1) and A(e, x2) and t1 <= t2 + D(x2)-1 and t1 + D(x1)-1 >= t2) {
+                                    this->_solver.addBinary(~Lit(this->_props[x1][s][t1]),
+                                                            ~Lit(this->_props[x2][s][t1]));
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    };
+    // Un professeur donne au plus un examen à chaque moment
+    _constraints["prof_temps_max"] = [this]() {
+        FOR(p, 1, this->_specs.P) {
+            FOR(t1, 1, this->_specs.T) {
+                FOR(t2, 1, this->_specs.T) {
+                    FOR(s, 1, this->_specs.S) {
+                        FOR(x1, 1, this->_specs.X) {
+                            FOR(x2, x1 + 1, this->_specs.X) {
+                                if (B(p, x1) and B(p, x2) and t1 <= t2 + D(x2)-1 and t1 + D(x1)-1 >= t2) {
+                                    this->_solver.addBinary(~Lit(this->_props[x1][s][t1]),
+                                                            ~Lit(this->_props[x2][s][t1]));
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    };
+    _constraints["examen_temps_max"] = [this]() {
+        FOR(x, 1, this->_specs.X) {
+            FOR(s, 1, this->_specs.S) {
+                FOR(t, this->_specs.T - D(x) + 1, this->_specs.T) {
+                    this->_solver.addUnit(~Lit(this->_props[x][s][t]));
+                }
+            }
+        }
     };
 }
