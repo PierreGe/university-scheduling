1e088b6cd75816d67c518f61185a3ff6c2e04cf8
Pierre Gerard <piegerar@ulb.ac.be>
2015-05-14 14:01:34 +0200

    [add] problem 6

---
+++ src/Problem6.cpp
@@ -0,0 +1,11 @@
+#include "Problem6.hpp"
+
+#include <iostream>
+#include <cassert>
+
+
+
+void Problem6::setConstraints(){
+    Problem5::setConstraints();
+
+}
---
+++ src/Problem6.hpp
@@ -0,0 +1,22 @@
+#ifndef __PROBLEM6_HPP
+#define __PROBLEM6_HPP
+
+#include <unordered_map>
+#include <functional>
+#include <string>
+
+#include "parser/SchedSpec.hpp"
+#include "minisat/Solver.hpp"
+
+#include "Problem5.hpp"
+
+class Problem6 : public Problem5 {
+public:
+    using Problem5::Problem5;
+protected:
+    virtual void setConstraints();
+public:
+    // virtual void test();
+};
+
+#endif // __PROBLEM_HPP
